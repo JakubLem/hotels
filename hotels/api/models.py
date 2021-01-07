83d1f0fce9e88b7f86dceeed10f0cb141b625c75
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-01-08 00:31:01 +0100

    H1 add some models

--- hotels/api/models.py
+++ hotels/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='cities')
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    birthdate = models.DateField()
+    email = models.EmailField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='persons')
+
+
+class Owner(Person):
+    start_date = models.DateField()
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    date_create = models.DateField()
+    city =  models.ForeignKey(Country, on_delete=models.CASCADE, related_name='hotels')
+    owner = models.OneToOneField(Owner)
+
+
+class Employee(Person):
+    ROLES = {
+        'S': 'S',
+        'T': 'T'
+    }
+
+    role = models.CharField(choices=ROLES)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='employees')
+
+
+class Family(models.Model):
+    surname = models.CharField(max_length=100)
+
+
+class Client(Person):
+    family = models.ForeignKey(Family, on_delete=models.CASCADE, related_name='clients')
